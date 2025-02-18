082480ccc317e947e7c588a96f66da956b90337a
Chidiebube Iroezindu <chidiebubeiroezindu@gmail.com>
2025-02-18 12:25:11 +0100

    add new theme colors, update pubspec for flutter_svg, and enhance home screen layout

--- lib/core/themes.dart
+++ lib/core/themes.dart
@@ -2,11 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 final ThemeData appTheme = ThemeData(
-  useMaterial3: true,
-  scaffoldBackgroundColor: SAColors.white,
-  fontFamily: GoogleFonts.inter().fontFamily
-);
-
+    useMaterial3: true,
+    scaffoldBackgroundColor: SAColors.white,
+    fontFamily: GoogleFonts.poppins().fontFamily);
 
 class SAColors {
   static const Color primary = Color(0xFFC95372);
@@ -14,4 +12,6 @@ class SAColors {
   static const Color black = Color(0xFF231515);
   static const Color white = Color(0xFFD6CACA);
   static const Color fontColor = Color(0xFF90A1B5);
+  static const Color babyBlue = Color(0xFFBCC3CD);
+  static const Color pink = Color(0xFFCE6C85);
 }
--- lib/presentation/screens/home_screen.dart
+++ lib/presentation/screens/home_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:social_adventure_ui/core/themes.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -10,6 +12,103 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      body: Stack(
+        alignment: Alignment.center,
+        children: [
+          Column(
+            children: [
+              Container(
+                height: MediaQuery.sizeOf(context).height * .5,
+                decoration: const BoxDecoration(
+                  color: SAColors.primary,
+                ),
+                child: Center(
+                  child: Stack(
+                    alignment: Alignment.center,
+                    children: [
+                      Container(
+                        transform: Matrix4.identity()..rotateZ(-5),
+                        transformAlignment: Alignment.center,
+                        height: 190,
+                        width: 190,
+                        decoration: const BoxDecoration(
+                          color: SAColors.pink,
+                        ),
+                      ),
+                      Image.asset(
+                        'lib/presentation/images/sa_ui_illustration-removebg.png',
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 60.0),
+                child: Column(
+                  children: [
+                    Container(
+                      padding: const EdgeInsets.all(6),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(15),
+                        color: SAColors.babyBlue,
+                      ),
+                      child: Text(
+                        'Step 03',
+                        style: Theme.of(context)
+                            .textTheme
+                            .labelLarge
+                            ?.copyWith(color: SAColors.secondary),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 20.0),
+                      child: Text(
+                        'Your Social\nAdventure Begins\nHere',
+                        textAlign: TextAlign.center,
+                        style: Theme.of(context)
+                            .textTheme
+                            .displaySmall
+                            ?.copyWith(fontWeight: FontWeight.w900),
+                      ),
+                    ),
+                    ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        elevation: 0,
+                        padding: const EdgeInsets.all(15),
+                        backgroundColor: SAColors.secondary,
+                        shape: const RoundedRectangleBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(23)),
+                        ),
+                      ),
+                      onPressed: () {},
+                      child: const Icon(
+                        Icons.arrow_forward,
+                        color: SAColors.white,
+                        size: 35,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+          SizedBox(
+            height: 100,
+            width: 100,
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(20),
+              child: Opacity(
+                opacity: .85,
+                child: SvgPicture.asset(
+                  'lib/presentation/images/four_point_star.svg',
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
